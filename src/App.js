77bead397aa3cc732a900fce56740ebfee3ecdd8
ahmad swalih <ahmadswalih777@gmail.com>
2022-06-03 09:16:36 +0530

    moviecard component created and added mapping through the moviecard in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import {useEffect} from 'react'
+import {useEffect,useState} from 'react'
 import SearchIcon from './Search.svg'
+import MovieCard from './components/MovieCard';
 import './App.css';
 
 const App = ()=> {
+
+const [movies,setMovies] = useState([])
 
 const searchMovies = async(title)=>{
   const api = await fetch(`http://omdbapi.com/?apikey=9eb261b1&s=${title}`)
   const data = await api.json()
    
-  console.log(data);
+  setMovies(data.Search);
 }
 
 useEffect(()=>{
@@ -30,6 +33,24 @@ useEffect(()=>{
         onClick={()=>{}}
       />
     </div>
+    {
+      movies.length > 0 ?
+      (<div className='container'>
+      {
+        movies.map((movie)=>{
+          return(
+            <MovieCard movie={movie}/>
+          )
+        })
+      }
+    </div>
+      ):(
+        <div className='empty'>
+          <h2>No Movies Found</h2>
+        </div>
+      ) 
+    }
+    
     </div>
   );
 }
